f0f9fc469725c444265b0b3a674e089e8494043b
KazuyaFki <complete501xx@yahoo.co.jp>
2020-03-23 11:20:53 +0900

    Initial Commit

---
+++ BMIcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMIcalc
+//
+//  Created by Kazuya Fukui on 2020/03/23.
+//  Copyright © 2020 Kazuya Fukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
